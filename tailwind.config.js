c780f23cdae0178d7b43da4bf0f6e148d3496321
Ricardo Matias <ricardo.matias0@gmail.com>
2021-02-12 11:03:27 +0100

    1.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: true,
+    content: ["./*.html"],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        title: ["Work Sans", ...defaultTheme.fontFamily.sans],
+        body: ["Gothic A1", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},
